dee5a081710e6ac1f99c68dd9761feb2489d1bd0
ananthamuruga <ananthamuruga@gmail.com>
2019-06-22 04:16:26 +0530

    Answers - Updated for NumberOfDaysInMonthForAYear

--- Anantha/Coding/src/main/java/codingchallenges/answers/NumberOfMonthDays.java
+++ Anantha/Coding/src/main/java/codingchallenges/answers/NumberOfMonthDays.java
@@ -1,18 +1,25 @@
 package codingchallenges.answers;
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class NumberOfMonthDays {
 
-	public static Calendar calendar;
 	public static void findMaxDaysofMonth(int month, int year) {
 		// Here, we are pointing to day 1 of the month to find the total days available in the month
-		int date = 1;	
+		int date = 1;
+		Format formatter = new SimpleDateFormat("MMMM");        
+        String monthName = formatter.format(new Date(year, month-1, 01)); // month in Java started from 0 instead of 1
+		
+		// instance of calendar object
+		Calendar calendar = Calendar.getInstance();
 		
 		if(month>-1 && month<12) {
 			// Setting date calendar date format as yyyy-mm-dd
-	        calendar.set(year, month-1, date); // month in Java started from 0 instead of 1
+	        calendar.set(year, month-1, date); 
 	        int maxDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
-	        System.out.println("Max Day in month "+Calendar.MONTH+" is: " + maxDay);
+	        System.out.println("Max Day in "+monthName+" month of year "+year+" is: " + maxDay);
 		}
 		else
 		{
@@ -23,10 +30,7 @@ public class NumberOfMonthDays {
 	
 	public static void main(String[] args) {
 		int mm=02,year=2016;
-        
-		// instance of calendar object
-        calendar = Calendar.getInstance();
-        
-        findMaxDaysofMonth(mm,year);        
+		
+        findMaxDaysofMonth(mm,year);  
     }
 }
--- Anantha/Coding/src/main/java/codingchallenges/answers/NumberOfMonthDaysTraditionalWay.java
+++ Anantha/Coding/src/main/java/codingchallenges/answers/NumberOfMonthDaysTraditionalWay.java
@@ -1,35 +1,48 @@
 package codingchallenges.answers;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class NumberOfMonthDaysTraditionalWay {
 	public static int number_Of_DaysInMonth=0;
 	public static void findNumberOfDaysInMonth(int month, int year) {
 		
-		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 )
-		{
-			number_Of_DaysInMonth = 31; 	
-		}
-		else if ( month == 4 || month == 6 || month == 9 || month == 11 )
-		{
-			number_Of_DaysInMonth = 30;	
-		}  
-		else if (month==2) // If the month is February
-		{
-			// Finding if the year provided is a leap year or not
-			if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
-	            number_Of_DaysInMonth = 29;
-	        } else {
-	            number_Of_DaysInMonth = 28;
-	        }
-		}			
-		else {
+		Format formatter = new SimpleDateFormat("MMMM");        
+		
+		// month in Java started from 0 instead of 1
+		// Here, we are pointing to day 1 of the month to find the total days available in the month
+        String monthName = formatter.format(new Date(year, month-1, 01)); 
+        
+        if(month>0 && month<13) {
+        	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 )
+    		{
+    			number_Of_DaysInMonth = 31; 	
+    		}
+    		else if ( month == 4 || month == 6 || month == 9 || month == 11 )
+    		{
+    			number_Of_DaysInMonth = 30;	
+    		}  
+    		else if (month==2) // If the month is February
+    		{
+    			// Finding if the year provided is a leap year or not
+    			if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+    	            number_Of_DaysInMonth = 29;
+    	        } else {
+    	            number_Of_DaysInMonth = 28;
+    	        }
+    		}
+        	System.out.println("Max Day in "+monthName+" month of year "+year+" is: " + number_Of_DaysInMonth);
+        }
+        else 
+        {
 			System.out.println("Please provide Valid Number between 1 to 12");
 			System.exit(0);
 		}		
 	}
 	
 	public static void main(String[] args) {
-		int mm=0, year =2016;
-		findNumberOfDaysInMonth(mm,year);
-		System.out.println("Number of days in "+mm+" is: "+number_Of_DaysInMonth);
+		int mm=2, year =2016;
+		findNumberOfDaysInMonth(mm,year);		
 	}
 }
